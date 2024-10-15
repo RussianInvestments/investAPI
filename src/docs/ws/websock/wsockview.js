fd24f5a2336a74331780a76a8463964d0e9b7ce5
r977ac <142778593+p977ac@users.noreply.github.com>
2024-10-15 11:46:34 +0300

    ws_update

--- src/docs/ws/websock/wsockview.js
+++ src/docs/ws/websock/wsockview.js
@@ -1,9 +1,9 @@
-import { useQuasar } from 'Quasar'
-import { useStore } from 'vuex'
-import { ref, onUpdated } from 'vue'
+import { useQuasar } from "Quasar";
+import { useStore } from "vuex";
+import { ref, onUpdated } from "vue";
 
-const WSockView  = {
-      template: `
+const WSockView = {
+  template: `
         <div class="q-pa-md">
         <q-layout view="hHh Lpr lff" container style="height: 700px" class="rounded-borders">
             <q-header elevated>
@@ -104,132 +104,142 @@ const WSockView  = {
 
         </div>
       `,
-        data () {
-            return {
-              token: null,
-              leftDrawerOpen: true,
-              errorShow: true,
-              splitterModel: 30,
-            }
-          },
-        computed: {
-          state () {
-            return this.$store.state.connection_state
-          },
-          stateText() {
-            return (this.$store.state.connection_state == 0)? 'Connected': 'Disconnected'
-          },
-          isNotConnected() {
-            return (this.$store.state.connection_state != 0)
-          },
-          isConnected() {
-                      return (this.$store.state.connection_state == 0)
-                    },
-          isPending() {
-             return (this.$store.state.connection_state == 1)
-          },
-          isError() {
-            return (this.$store.state.error != null)
-          },
-          textOut() {
-            var s = '';
-            this.$store.state.responses.forEach(resp=>s =  JSON.stringify(resp, null, 2)+ '\n\n' + s )
-            return s;
-          },
-          streamLog() {
-            return this.$store.state.streamLog.slice(-1000).reverse();
-          },
-          methodName() {
-            return this.$route.meta.methodName;
-          },
-          methods() {
-            return [
-            '/tinkoff.public.invest.api.contract.v1.MarketDataStreamService/MarketDataStream',
-            '/tinkoff.public.invest.api.contract.v1.OperationsStreamService/PortfolioStream',
-            '/tinkoff.public.invest.api.contract.v1.OperationsStreamService/PositionsStream',
-            '/tinkoff.public.invest.api.contract.v1.OrdersStreamService/TradesStream',
-            '/tinkoff.public.invest.api.contract.v1.OrdersStreamService/OrderStateStream',                  
-                  
-
-            ]
-          },
-          request: {
-            get() {
-              return this.$store.state.request.json;
-                             },
-            set(value) {
-                this.$store.commit('updateRequest', value)
-            }
-          },
-          error() {
-            return (this.$store.state.error && this.$store.state.error.target)?
-                (this.$store.state.error.target.constructor)?"Unexpected error on " + (this.$store.state.error.target.constructor.name)
-                    : this.$store.state.error
-                : (this.$store.state.error)?this.$store.state.error:null;
-          }
-        },
-        methods: {
-            doSend() {
-                if (!this.$store.state.token) {
-                    this.$store.commit('onError', 'Token not set. Please setup token in settings');
-                    this.notifyError('Token not set. Please setup token in settings')
-                    this.$store.commit('onDisconnect');
-                    return false;
-                }
-                this.$store.dispatch('connect', this.request)
-            },
-            doDisconnect() {
-                this.$store.dispatch('disconnect', this.request)
-            }
-        }
-    }
-
-
+  data() {
+    return {
+      token: null,
+      leftDrawerOpen: true,
+      errorShow: true,
+      splitterModel: 30,
+    };
+  },
+  computed: {
+    state() {
+      return this.$store.state.connection_state;
+    },
+    stateText() {
+      return this.$store.state.connection_state == 0
+        ? "Connected"
+        : "Disconnected";
+    },
+    isNotConnected() {
+      return this.$store.state.connection_state != 0;
+    },
+    isConnected() {
+      return this.$store.state.connection_state == 0;
+    },
+    isPending() {
+      return this.$store.state.connection_state == 1;
+    },
+    isError() {
+      return this.$store.state.error != null;
+    },
+    textOut() {
+      var s = "";
+      this.$store.state.responses.forEach(
+        (resp) => (s = JSON.stringify(resp, null, 2) + "\n\n" + s)
+      );
+      return s;
+    },
+    streamLog() {
+      return this.$store.state.streamLog.slice(-1000).reverse();
+    },
+    methodName() {
+      return this.$route.meta.methodName;
+    },
+    methods() {
+      return [
+        "/tinkoff.public.invest.api.contract.v1.MarketDataStreamService/MarketDataStream",
+        "/tinkoff.public.invest.api.contract.v1.OperationsStreamService/PortfolioStream",
+        "/tinkoff.public.invest.api.contract.v1.OperationsStreamService/PositionsStream",
+        "/tinkoff.public.invest.api.contract.v1.OrdersStreamService/TradesStream",
+        "/tinkoff.public.invest.api.contract.v1.OrdersStreamService/OrderStateStream",
+      ];
+    },
+    request: {
+      get() {
+        return this.$store.state.request.json;
+      },
+      set(value) {
+        this.$store.commit("updateRequest", value);
+      },
+    },
+    error() {
+      return this.$store.state.error && this.$store.state.error.target
+        ? this.$store.state.error.target.constructor
+          ? "Unexpected error on " +
+            this.$store.state.error.target.constructor.name
+          : this.$store.state.error
+        : this.$store.state.error
+        ? this.$store.state.error
+        : null;
+    },
+  },
+  methods: {
+    doSend() {
+      if (!this.$store.state.token) {
+        this.$store.commit(
+          "onError",
+          "Token not set. Please setup token in settings"
+        );
+        this.notifyError("Token not set. Please setup token in settings");
+        this.$store.commit("onDisconnect");
+        return false;
+      }
+      this.$store.dispatch("connect", this.request);
+    },
+    doDisconnect() {
+      this.$store.dispatch("disconnect", this.request);
+    },
+  },
+};
 
 export default {
-    setup() {
-        const $q = useQuasar();
-        const $store = useStore();
-        function setupSecret() {
-            $q.dialog({
-                    title: 'Settings',
-                    message: 'Enter auth token',
-                    prompt: {
-                      model: $store.state.token,
-                      type: 'text'
-                    },
-                    cancel: true,
-                    persistent: true
-                  }).onOk(data => {
-                    $store.commit('onAuthToken', data)
-                    // console.log('>>>> OK, received', data)
-                  }).onCancel(() => {
-                    // console.log('>>>> Cancel')
-                  }).onDismiss(() => {
-                    // console.log('I am triggered on both OK and Cancel')
-                  })
-        }
-        function notifyOk(msg) {
-            $q.notify({
-                      type: 'positive',
-                      position: 'top-right',
-                      message: msg
-                    })
-        }
-        function notifyError(error) {
-                 $q.notify({
-                      type: 'negative',
-                      position: 'top-right',
-                      message: error
-                    })
-        }
-        onUpdated(() => {
-            if ($store.state.error) {
-                notifyError($store.state.error)
-            }
+  setup() {
+    const $q = useQuasar();
+    const $store = useStore();
+    function setupSecret() {
+      $q.dialog({
+        title: "Settings",
+        message: "Enter auth token",
+        prompt: {
+          model: $store.state.token,
+          type: "text",
+        },
+        cancel: true,
+        persistent: true,
+      })
+        .onOk((data) => {
+          $store.commit("onAuthToken", data);
+          // console.log('>>>> OK, received', data)
+        })
+        .onCancel(() => {
+          // console.log('>>>> Cancel')
+        })
+        .onDismiss(() => {
+          // console.log('I am triggered on both OK and Cancel')
         });
-        $store.commit('init', {notifyOk, notifyError})
-        return {setupSecret, notifyOk, notifyError};
-    }, ...WSockView
-
-}
+    }
+    function notifyOk(msg) {
+      $q.notify({
+        type: "positive",
+        position: "top-right",
+        message: msg,
+      });
+    }
+    function notifyError(error) {
+      $q.notify({
+        type: "negative",
+        position: "top-right",
+        message: error,
+      });
+    }
+    onUpdated(() => {
+      if ($store.state.error) {
+        notifyError($store.state.error);
+      }
+    });
+    $store.commit("init", { notifyOk, notifyError });
+    return { setupSecret, notifyOk, notifyError };
+  },
+  ...WSockView,
+};
